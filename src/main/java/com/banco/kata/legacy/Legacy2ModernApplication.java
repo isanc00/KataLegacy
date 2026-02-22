72fe53e60044135bf1b7273bedbd403434b89380
Ivan <leosnchz.ilst@gmail.com>
2026-02-22 12:03:18 -0500

    ADD: initialize Maven project

---
+++ src/main/java/com/banco/kata/legacy/Legacy2ModernApplication.java
@@ -0,0 +1,13 @@
+package com.banco.kata.legacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Legacy2ModernApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Legacy2ModernApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banco/kata/legacy/Legacy2ModernApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.kata.legacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Legacy2ModernApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
