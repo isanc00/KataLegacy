9c9b5873a2196d50e78eb262d6607c6b535e17c6
Ivan <leosnchz.ilst@gmail.com>
2026-02-23 17:17:00 -0500

    ADD: styles to presentation layout

--- demo-front/src/app/app.component.ts
+++ demo-front/src/app/app.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
+import { MigrationService } from './services/migration.service';
+import { LoaderService } from './shared/services/loader.service';
+import { LoaderComponent } from './shared/components/loader.component';
+import { randomInt } from 'node:crypto';
+import { MigrationRequest, MigrationResponse } from './models/models';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule, MonacoEditorModule],
+  imports: [FormsModule, MonacoEditorModule, LoaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  private migrationService = inject(MigrationService);
+  private loaderService = inject(LoaderService);
+
+
   editorOptionsCobol = {
     theme: 'vs-dark',
     language: 'cobol',
@@ -26,26 +34,53 @@ export class AppComponent {
   sourceLanguage = 'COBOL';
   targetLanguage = 'JAVA';
 
-  cobolCode =
+  legacyCode =
     'IF AMOUNT > 0\n  DISPLAY "VALID"\nELSE\n  DISPLAY "INVALID"\nEND-IF';
-  javaCode = '// El código modernizado aparecerá aquí...';
+  targetCode = '// El código modernizado aparecerá aquí...';
 
   traducir() {
-    console.log('Traduciendo el siguiente código:\n', this.cobolCode);
-    // Aquí irá la llamada al servicio HTTP
+    console.log('Traduciendo el siguiente código:\n', this.legacyCode);
+    this.targetLanguage = 'JAVA'
+    this.targetCode = '// Código traducido de COBOL a JAVA aparecerá aquí...';
+
+    this.loaderService.show();
+
+    const request: MigrationRequest  = {
+      transactionCode: 'TRX-001'+ crypto.randomUUID(),
+      sourceLanguage: this.sourceLanguage,
+      targetLanguage: this.targetLanguage,
+      legacyCode: this.legacyCode,
+    };
+
+    this.migrationService.translateCode(request).subscribe({
+      next: (response) => {
+        console.log('Respuesta del backend recibida:', response);
+        this.targetCode = response.transactionCode;
+        this.loaderService.hide();
+      },
+      error: (error) => {
+        console.error('Error al traducir el código:', error);
+        this.targetCode = '// Error al traducir el código. Por favor, inténtalo de nuevo.';
+        this.loaderService.hide();
+      }
+    });
+
   }
 
   limpiar() {
-    this.cobolCode = '';
-    this.javaCode = '';
+    this.targetCode = '';
+    this.legacyCode = '';
+    console.log('Código de destino limpiado');
+    console.log('Código de origen limpiado');
   }
 
   onSourceChange() {
     console.log('Lenguaje de origen cambiado a:', this.sourceLanguage);
+    this.loaderService.show();
   }
 
   onTargetChange() {
     console.log('Lenguaje de destino cambiado a:', this.targetLanguage);
-
+    this.loaderService.hide();
   }
 }
