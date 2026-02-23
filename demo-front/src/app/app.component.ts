159898982e7fbc34f058add79a2098e916a6d570
Ivan <leosnchz.ilst@gmail.com>
2026-02-23 15:13:16 -0500

    ADD: design for presentation layout

--- demo-front/src/app/app.component.ts
+++ demo-front/src/app/app.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FormsModule, MonacoEditorModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'demo-front';
+  editorOptionsCobol = {
+    theme: 'vs-dark',
+    language: 'cobol',
+    automaticLayout: true,
+  };
+  editorOptionsJava = {
+    theme: 'vs-dark',
+    language: 'java',
+    readOnly: true,
+    automaticLayout: true,
+  };
+
+  sourceLanguage = 'COBOL';
+  targetLanguage = 'JAVA';
+
+  cobolCode =
+    'IF AMOUNT > 0\n  DISPLAY "VALID"\nELSE\n  DISPLAY "INVALID"\nEND-IF';
+  javaCode = '// El código modernizado aparecerá aquí...';
+
+  traducir() {
+    console.log('Traduciendo el siguiente código:\n', this.cobolCode);
+    // Aquí irá la llamada al servicio HTTP
+  }
+
+  limpiar() {
+    this.cobolCode = '';
+    this.javaCode = '';
+  }
+
+  onSourceChange() {
+    console.log('Lenguaje de origen cambiado a:', this.sourceLanguage);
+  }
+
+  onTargetChange() {
+    console.log('Lenguaje de destino cambiado a:', this.targetLanguage);
+
+  }
 }
