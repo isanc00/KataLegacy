324997aebd34d22da93d943a727d7845a7a94d8b
Ivan <leosnchz.ilst@gmail.com>
2026-02-23 20:26:24 -0500

    ADD: info section

--- demo-front/src/app/app.component.ts
+++ demo-front/src/app/app.component.ts
@@ -18,6 +18,13 @@ export class AppComponent {
   private migrationService = inject(MigrationService);
   private loaderService = inject(LoaderService);
 
+  appliedRules: string[] = [];
+  correlationId: string = '';
+  executionTimeMs: string = '0';
+  modernCode: string = '';
+  status: string = '';
+  transactionCode: string = '';
+  warnings: string[] = [];
 
   editorOptionsCobol = {
     theme: 'vs-dark',
@@ -40,13 +47,13 @@ export class AppComponent {
 
   traducir() {
     console.log('Traduciendo el siguiente código:\n', this.legacyCode);
-    this.targetLanguage = 'JAVA'
+    this.targetLanguage = 'JAVA';
     this.targetCode = '// Código traducido de COBOL a JAVA aparecerá aquí...';
 
     this.loaderService.show();
 
-    const request: MigrationRequest  = {
-      transactionCode: 'TRX-001'+ crypto.randomUUID(),
+    const request: MigrationRequest = {
+      transactionCode: 'TRX-001' + crypto.randomUUID(),
       sourceLanguage: this.sourceLanguage,
       targetLanguage: this.targetLanguage,
       legacyCode: this.legacyCode,
@@ -55,32 +62,51 @@ export class AppComponent {
     this.migrationService.translateCode(request).subscribe({
       next: (response) => {
         console.log('Respuesta del backend recibida:', response);
-        this.targetCode = response.transactionCode;
+
+        if(response.status != 'FAILED') {
+          this.appliedRules = response.appliedRules || [];
+          this.correlationId = response.correlationId;
+          this.executionTimeMs = response.executionTimeMs;
+          this.modernCode = response.modernCode;
+          this.status = response.status;
+          this.transactionCode = response.transactionCode;
+          this.warnings = response.warnings || [];
+        }else {
+          this.status = 'FAILED';
+          this.modernCode = '// La traducción ha fallado. Por favor, revisa el código de origen e inténtalo de nuevo. Si el problema persiste, contacta al soporte técnico.\n\nDetalles del error:\n' + (response.warnings ? response.warnings.join('\n') : 'No se proporcionaron detalles adicionales.');
+        }
+        this.targetCode = this.modernCode;
         this.loaderService.hide();
+
+
       },
       error: (error) => {
         console.error('Error al traducir el código:', error);
-        this.targetCode = '// Error al traducir el código. Por favor, inténtalo de nuevo.';
+        this.targetCode =
+          '// Error al traducir el código. Por favor, inténtalo de nuevo.';
         this.loaderService.hide();
-      }
+      },
     });
-
   }
 
   limpiar() {
-    this.targetCode = '';
-    this.legacyCode = '';
-    console.log('Código de destino limpiado');
-    console.log('Código de origen limpiado');
+    this.targetCode = '// El código modernizado aparecerá aquí...';
+    this.legacyCode = '// Escribe tu código '+ this.sourceLanguage+' aquí...';
+    this.appliedRules = [];
+    this.correlationId = '';
+    this.executionTimeMs = '0';
+    this.modernCode = '';
+    this.status = '';
+    this.transactionCode = '';
+    this.warnings = [];
+    console.log('Valores reseteados. Listo para una nueva traducción.');
   }
 
   onSourceChange() {
     console.log('Lenguaje de origen cambiado a:', this.sourceLanguage);
-    this.loaderService.show();
   }
 
   onTargetChange() {
     console.log('Lenguaje de destino cambiado a:', this.targetLanguage);
-    this.loaderService.hide();
   }
 }
